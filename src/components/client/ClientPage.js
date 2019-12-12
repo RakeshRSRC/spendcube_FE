7b05c5bd8af750de60bb3ef627c9764068514378
gauniyal rakesh <gauniyal.rakesh@bcg.com>
2019-12-12 20:31:01 +0530

    RSRC: Started working on Client page.

--- src/components/client/ClientPage.js
+++ src/components/client/ClientPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import './ClientMainCss.css';
 
 export default class ClientPage extends React.Component {
     constructor(props) {
@@ -8,7 +11,26 @@ export default class ClientPage extends React.Component {
 
     render() {
         return (
-            <h1> Welcome to Client Page.... </h1>
+            <Tabs className="client-tab-container">
+                <TabList>
+                    <Tab>Overview</Tab>
+                    <Tab>Map View</Tab>
+                    <Tab>Product View</Tab>
+                    <Tab>Incoterm View</Tab>
+                </TabList>
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 3</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 4</h2>
+                </TabPanel>
+            </Tabs>
         )
     }
 }
